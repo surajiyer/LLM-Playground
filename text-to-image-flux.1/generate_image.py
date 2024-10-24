fb1179af71c8cb7eb8df40b134f103ba8697a434
Suraj Iyer <suraj.iyer_external@wartsila.com>
2024-10-24 02:02:05 +0200

    added controlnet img2img generation

--- text-to-image-flux.1/generate_image.py
+++ text-to-image-flux.1/generate_image.py
@@ -1,6 +1,6 @@
 import time
 from huggingface_hub import hf_hub_download
-from mflux import Flux1, Config
+from mflux import ConfigControlnet, Flux1, Config, Flux1Controlnet
 from mflux.config.model_config import ModelConfig
 
 
@@ -11,6 +11,7 @@ class ImageGenerator:
         self.model_alias = model_alias
         self.quantize = quantize
         self.flux = None
+        self.controlnet = None
 
     def download_lora_file(self):
         return hf_hub_download(repo_id=self.repo_id, filename=self.lora_file_name)
@@ -20,43 +21,68 @@ class ImageGenerator:
             model_config=ModelConfig.from_alias(self.model_alias),
             quantize=self.quantize,
             lora_paths=[lora_file_path],
-            lora_scales=[1.0],
+            lora_scales=[0.95],
+        )
+        self.controlnet = Flux1Controlnet(
+            model_config=ModelConfig.from_alias(self.model_alias),
+            quantize=self.quantize,
+            lora_paths=[lora_file_path],
+            lora_scales=[0.95],
         )
 
-    def generate_image(self, seed, prompt, num_inference_steps, height, width, output_path):
+    def generate_image(self, num_inference_steps, width, height, seed, prompt, output_path, controlnet_image_path=None):
         if self.flux is None:
             raise ValueError("Model is not loaded. Call load_model() first.")
 
-        config = Config(
-            num_inference_steps=num_inference_steps,
-            height=height,
-            width=width,
-        )
+        if controlnet_image_path is not None:
+            config = ConfigControlnet(
+                num_inference_steps=num_inference_steps,
+                width=width,
+                height=height,
+                guidance=3.5,
+                controlnet_strength=0.75,
+            )
 
-        t0 = time.time()
-        image = self.flux.generate_image(
-            seed=seed,
-            prompt=prompt,
-            config=config
-        )
+            t0 = time.time()
+            image = self.controlnet.generate_image(
+                seed=seed,
+                prompt=prompt,
+                output=output_path,
+                controlnet_image_path=controlnet_image_path,
+                config=config,
+            )
+        else:
+            config = Config(
+                num_inference_steps=num_inference_steps,
+                width=width,
+                height=height,
+            )
+
+            t0 = time.time()
+            image = self.flux.generate_image(
+                seed=seed,
+                prompt=prompt,
+                config=config
+            )
         print(f"Generation time: {time.time() - t0:.2f}s")
 
         image.save(path=output_path)
 
 
 if __name__ == "__main__":
-    repo_id = "aleksa-codes/flux-ghibsky-illustration"
-    lora_file_name = "lora.safetensors"
+    repo_id = "prithivMLmods/Castor-Character-Polygon-Flux-LoRA"
+    lora_file_name = "Castor-Character-Polygon-LoRA.safetensors"
     output_path = "data/image.png"
 
-    generator = ImageGenerator(repo_id, lora_file_name)
+    generator = ImageGenerator(repo_id, lora_file_name, model_alias="dev")
     lora_file_path = generator.download_lora_file()
     generator.load_model(lora_file_path)
     generator.generate_image(
-        seed=43,
-        prompt="GHIBSKY style, the most beautiful place in the universe",
-        num_inference_steps=3,
-        height=1024,
+        num_inference_steps=28,
         width=1024,
+        height=1024,
+        seed=3973736786,
+        prompt="Create a 3D polygon character portrait of a young athletic Indian man in his early 20s. He has short, curly black hair, dark brown eyes, and a bearded face. His muscular build is visible through a sleeveless workout shirt, and his skin is glowing with sweat, hinting at recent physical activity. His expression is focused and intense. The lighting is dynamic, highlighting the contours of his muscles and the texture of his skin. The background is a garden setting, slightly out of focus",
+        controlnet_image_path="image.webp",
         output_path=output_path
     )
